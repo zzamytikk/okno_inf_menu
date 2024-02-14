47ad1783b8c25d0377be06f93cdb239860c6693f
zzamytikk <160018221+zzamytikk@users.noreply.github.com>
2024-02-14 20:00:05 +0200

    Add files via upload

---
+++ z.js
@@ -0,0 +1,13 @@
+var z = {
+  $:q => {
+    //document.querySelectorAll('#id')[0].innerHTML='querySelectorAll';
+    //console.error('querySelector', document.querySelector('#id'));//.innerHTML='querySelector';
+    //document.getElementById('id').innerHTML='123';
+    
+    //console.error();
+    
+    $('#id').html('321');
+  }
+};
+  //console.error('start');
+z.$();
